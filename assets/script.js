20ad7f90d84d581ba6437155afd48a1382b39972
SurendarRUS <surendarprime1@gmail.com>
2022-06-18 22:30:08 +0530

    Initial Commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function navOpen() {
+    var x = document.getElementById("menuItems");
+    if(x.style.display==="none"){
+        x.style.display="block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
